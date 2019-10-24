02229a071636043de9fa2adb6cd4b8e31209e7f0
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-10-24 21:23:08 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import ( 
+	"fmt"
+
+	"github.com/YosukeCSato/go-example"
+)
+
+func main() {
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.MonkeyFeed())
+	fmt.Println(animals.RabbitFeed())
+}
