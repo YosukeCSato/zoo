0011596f77cc54ec69472e1d9600d659c8723f65
Yosuke C Sato <yosuke.c.sato@icloud.com>
2019-10-24 22:48:56 +0900

    テストの追加

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println(AppName()) /* 関数AppNameの呼び出し */
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.MonkeyFeed())
 	fmt.Println(animals.RabbitFeed())
